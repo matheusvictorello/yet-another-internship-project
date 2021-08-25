05ab25ac69c4158cf93dbe2810ba403166ca8fd3
Matheus Victorello <matheusvictorello@gmail.com>
2021-08-25 15:02:26 -0300

    Application class

---
+++ src/main/java/com/victorello/yetanotherinternshipproject/YetAnotherInternshipProjectApplication.java
@@ -0,0 +1,12 @@
+package com.victorello.yetanotherinternshipproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherInternshipProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetAnotherInternshipProjectApplication.class, args);
+    }
+}
---
+++ src/test/java/com/victorello/yetanotherinternshipproject/YetAnotherInternshipProjectApplicationTest.java
@@ -0,0 +1,12 @@
+package com.victorello.yetanotherinternshipproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherInternshipProjectApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
